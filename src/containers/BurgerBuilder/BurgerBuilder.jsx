6a65c9046e094ace475d6ec9a54bd96f618ec60c
Aron Bergman <brgmn@icloud.com>
2020-04-09 16:52:44 +0300

    Handling Errors

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -6,6 +6,7 @@ import Modal from "../../components/UI/Modal/Modal"
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 import axios from "./../../axios-orders";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 const INGRIDIENT_PRICES = {
     salad: .5,
@@ -143,4 +144,4 @@ class BurgerBuilder extends React.Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
