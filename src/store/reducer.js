1fb44180ba7897138b59b7b76f688d85697bb7b9
Aron Bergman <brgmn@icloud.com>
2020-04-14 17:51:17 +0300

    Finishing the Reducer for Ingredients

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,12 @@
 import * as actionTypes from './actions'
 
 const initalState = {
-    ingridients: null,
+    ingridients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    },
     totalPrice: 4
 };
 
@@ -9,11 +14,23 @@ const reducer = (state = initalState, action) => {
     switch (action.type) {
         case actionTypes.ADD_INGRIDIENTS:
             return {
-
-            }
+                ...state,
+                ingridients: {
+                    ...state.ingridients,
+                    [action.ingridientName]: state.ingridients[action.ingridientName] + 1
+                }
+            };
+        case actionTypes.REMOVE_INGRIDIENTS:
+            return {
+                ...state,
+                ingridients: {
+                    ...state.ingridients,
+                    [action.ingridientName]: state.ingridients[action.ingridientName] - 1
+                }
+            };
+        default:
+            return state;
     }
-
-    return state;
 };
 
 export default reducer;
